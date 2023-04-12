39a8e2f7ba48d3d9bfb1d1a12905e2963ea1c596
MehrdadAbsolute <mehrdad.farokhy@absolute.de>
2023-04-12 09:58:20 +0200

    Revert "Revert "buttons go away""
    This reverts commit 84182312dce89bd6edfc78f911b4e07894bf6214.

--- Assets/Scripts/Connect.cs
+++ Assets/Scripts/Connect.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Connect : MonoBehaviour
 {
+    public GameObject hostbutton;
+    public GameObject clientbutton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +17,22 @@ public class Connect : MonoBehaviour
     public void Host()
     {
         NetworkManager.Singleton.StartHost();
+        Debug.Log("hello illia");
+        hostbutton.SetActive(false);
+        clientbutton.SetActive(false);
     }
 
     public void Client()
     {
         NetworkManager.Singleton.StartClient();
+        Debug.Log("Bye Illia");
+        clientbutton.SetActive(false);
+        hostbutton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
